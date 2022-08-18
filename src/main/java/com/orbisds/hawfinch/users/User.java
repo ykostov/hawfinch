16d510c464494ae709a3bb1a943ed9d353f2cd37
Yasen Kostov <qskatak04@gmail.com>
2022-08-18 15:58:12 +0300

    create Many_to_Many relationship and JoinTable

--- src/main/java/com/orbisds/hawfinch/users/User.java
+++ src/main/java/com/orbisds/hawfinch/users/User.java
@@ -1,17 +1,20 @@
 package com.orbisds.hawfinch.users;
 
 
+import com.orbisds.hawfinch.lang.Lang;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="users", schema = "public")
+@EnableAutoConfiguration
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
@@ -21,4 +24,16 @@ public class User {
     @Id
     private String user_id;
     private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name="langs_enrolled",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "lang_id")
+    )
+    private Set<Lang> enrolledLangs = new HashSet<>();
+
+    public void enrollLangs(Lang lang) {
+        enrolledLangs.add(lang);
+    }
 }
