21f1c3ba6df1f1e4f56810c972b780f9f068af70
Yasen Kostov <qskatak04@gmail.com>
2022-08-18 11:34:09 +0300

    create GET and POST requests from users

--- src/main/java/com/orbisds/hawfinch/users/UserController.java
+++ src/main/java/com/orbisds/hawfinch/users/UserController.java
@@ -1,6 +1,9 @@
 package com.orbisds.hawfinch.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -8,10 +11,17 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @RequestMapping("/hawfinch")
-    public String callme()
-    {
-        return "hello hawfinch";
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
     }
 
 }
---
+++ src/main/java/com/orbisds/hawfinch/users/UserService.java
@@ -0,0 +1,25 @@
+package com.orbisds.hawfinch.users;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+}
