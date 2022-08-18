79d1155e932b280a692fd293c37228b755d43f17
Yasen Kostov <qskatak04@gmail.com>
2022-08-18 10:22:21 +0300

    Initial commit

---
+++ src/main/java/com/orbisds/hawfinch/HawfinchApplication.java
@@ -0,0 +1,13 @@
+package com.orbisds.hawfinch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawfinchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawfinchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orbisds/hawfinch/HawfinchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbisds.hawfinch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawfinchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
