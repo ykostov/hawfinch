9e0e72ec9a89b05cedd29dddf46148a1668f29a4
Yasen Kostov <qskatak04@gmail.com>
2022-08-19 11:47:51 +0300

    Create ManyToOne relationship

--- src/main/java/com/orbisds/hawfinch/positions/Position.java
+++ src/main/java/com/orbisds/hawfinch/positions/Position.java
@@ -1,5 +1,8 @@
 package com.orbisds.hawfinch.positions;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.orbisds.hawfinch.lang.Lang;
+import com.orbisds.hawfinch.users.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +10,8 @@ import lombok.Setter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name="positions", schema = "public")
@@ -20,5 +25,9 @@ public class Position {
     @Id
     private String position_id;
     private String name;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "position")
+    private Set<User> users = new HashSet<>();
 
 }
